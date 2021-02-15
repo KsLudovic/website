c80c1a88c02666f2466fb4f6ef39dc5ab00ea685
Shaxomann <ludovic.casseyo@gmail.com>
2021-02-15 21:27:39 +0100

    add function

--- Terminal/Terminal.js
+++ Terminal/Terminal.js
@@ -262,6 +262,62 @@ $.terminal.defaults.formatters.push(function (str) {
 });
 
 function testFunc(data) {
-  $.terminal.active().echo(`${outputSign} ${data}`);
   console.log(data);
+  $.terminal.active().echo(`${outputSign} ${data}`);
+  
+}
+
+function sum(a,b) {
+   console.log(a + b);
+  $.terminal.active().echo(`${outputSign} sum = ${a} + ${b} = ${a + b}`);
+}
+
+function sub(a,b) {
+  console.log(a + b);
+ $.terminal.active().echo(`${outputSign} sub = ${a} - ${b} = ${a - b}`);
+}
+
+
+function multiply(a,b) {
+  console.log(a * b);
+ $.terminal.active().echo(`${outputSign} product = ${a} * ${b} = ${a * b}`);
+}
+
+function divide(a,b) {
+  console.log(a / b);
+ $.terminal.active().echo(`${outputSign} nettiquo = ${a} / ${b} = ${a / b}`);
+}
+
+function binary(a) {
+  console.log(a);
+ $.terminal.active().echo(`${outputSign} Binary Conversion = ${a} -> ${a.toString(2)}`);
+}
+
+function hex(a) {
+  console.log(a);
+ $.terminal.active().echo(`${outputSign} Hexa Conversion = ${a} -> ${a.toString(16)}`);
+}
+
+function power(a,b){
+  console.log(Math.pow(a,b));
+  $.terminal.active().echo(`${outputSign} result = ${a} ^ ${b} -> ${Math.pow(a,b)}`);
+}
+
+function factorial(a){
+  console.log(a);
+  const calcRange = (a) => {
+    return [...Array(a).keys()].map(i => i + 1)
+  }
+  if (a === 0) {
+    $.terminal.active().echo(`${outputSign} the factorial of 0 is 1.}`);
+  }
+  if (a < 0) {
+    $.terminal.active().echo(`${outputSign} Sorry, factorial does not exist for negative numbers.}`);
+  }
+  if (a > 0) {
+    const range = calcRange(a)
+    const factorial = range.reduce((a, c) => a * c, 1)
+    $.terminal.active().echo(`${outputSign} The factorial of ${a} is ${factorial}`);
+  }
+
 }
--- Terminal/Test.js
+++ Terminal/Test.js
@@ -16,3 +16,49 @@ function setup() {
   noCanvas();
   console.log("Yep!!!");
 }
+
+function multiply(a,b) {
+  console.log(a * b);
+ $.terminal.active().echo(`${outputSign} product = ${a} * ${b} = ${a * b}`);
+}
+
+function divide(a,b) {
+  console.log(a / b);
+ $.terminal.active().echo(`${outputSign} product = ${a} / ${b} = ${a / b}`);
+}
+
+function binary(a) {
+  console.log(a);
+ $.terminal.active().echo(`${outputSign} Binary Conversion = ${a} -> ${a.toString(2)}`);
+}
+
+function hex(a) {
+  console.log(a);
+ $.terminal.active().echo(`${outputSign} Hexa Conversion = ${a} -> ${a.toString(16)}`);
+}
+
+function power(a,b){
+  console.log(Math.pow(a,b));
+  $.terminal.active().echo(`${outputSign} result = ${a} ^ ${b} -> ${Math.pow(a,b)}`);
+}
+
+function factorial(a){
+  console.log(a);
+  const calcRange = (a) => {
+    return [...Array(a).keys()].map(i => i + 1)
+  }
+  if (a === 0) {
+    $.terminal.active().echo(`${outputSign} the factorial of 0 is 1.}`);
+  }
+  if (a < 0) {
+    $.terminal.active().echo(`${outputSign} Sorry, factorial does not exist for negative numbers.}`);
+  }
+  if (a > 0) {
+    const range = calcRange(a)
+    const factorial = range.reduce((a, c) => a * c, 1)
+    $.terminal.active().echo(`${outputSign} The factorial of ${a} is ${factorial}`);
+  }
+
+}
+
+
