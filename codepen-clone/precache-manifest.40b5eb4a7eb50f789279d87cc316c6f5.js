53feedd3073a6ad34c91722a037a32a068edf7e2
Ludovic_Casse <77667511+KsLudovic@users.noreply.github.com>
2021-02-23 16:34:22 +0100

    Add files via upload

---
+++ codepen-clone/precache-manifest.40b5eb4a7eb50f789279d87cc316c6f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee98cbb08376a3b124c97990f2401ebb",
+    "url": "/website/codepen-clone/index.html"
+  },
+  {
+    "revision": "70c3ff1a8dd6beff5ca9",
+    "url": "/website/codepen-clone/static/css/2.c46397a6.chunk.css"
+  },
+  {
+    "revision": "500318f9651fc74116bb",
+    "url": "/website/codepen-clone/static/css/main.7d2118c4.chunk.css"
+  },
+  {
+    "revision": "70c3ff1a8dd6beff5ca9",
+    "url": "/website/codepen-clone/static/js/2.4254f81d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/website/codepen-clone/static/js/2.4254f81d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "500318f9651fc74116bb",
+    "url": "/website/codepen-clone/static/js/main.0f64c0cf.chunk.js"
+  },
+  {
+    "revision": "13d89335ed6a9e08c9df",
+    "url": "/website/codepen-clone/static/js/runtime-main.68aec911.js"
+  }
+]);
